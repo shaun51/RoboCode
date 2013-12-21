201c691a95aaba92a443f945e4c5c041b0bc3956
shaun51 <shaun.nichols51@gmail.com>
2013-12-20 20:52:12 -0800

    First Commit

---
+++ MyProject/MyRobot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robocode;
+
+
+namespace SRN
+{
+    class MyRobot : Robot
+    {
+        public override void Run()
+        {
+            base.Run();
+        }
+    }
+}
